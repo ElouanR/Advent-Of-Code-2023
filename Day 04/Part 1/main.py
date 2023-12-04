6e71804265564160d6bccfa9f98b6e62053e58d7
Elouan <elouan.rigomont@epitech.eu>
2023-12-04 22:42:08 +0100

    [+] Day 04 Part 1 complete

--- Day 04/Part 1/main.py
+++ Day 04/Part 1/main.py
@@ -2,7 +2,31 @@
 # https://adventofcode.com/2023/day/4#part1
 
 def main():
-    pass
+    sum = 0
+
+    with open("Day 04/puzzle_input.txt", "r") as file:
+        lines = file.readlines()
+
+    lines = [line.strip().split(": ")[-1] for line in lines]
+
+    for i in range(len(lines)):
+        worth = 0
+
+        win_nbrs = lines[i].split(" | ")[0]
+        win_nbrs = [nbr for nbr in win_nbrs.split(" ") if nbr != '']
+        my_nbrs = lines[i].split(" | ")[1]
+        my_nbrs = [nbr for nbr in my_nbrs.split(" ") if nbr != '']
+
+        for j in range(len(my_nbrs)):
+            if my_nbrs[j] in win_nbrs:
+                if worth == 0:
+                    worth = 1
+                else:
+                    worth *= 2
+
+        sum += worth
+
+    print(sum)
 
 if __name__ == "__main__":
     main()
