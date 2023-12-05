899b942fae29fba08c9823ac5828cf1ee6e5f20e
Rigomont <elouan.rigomont@epitech.eu>
2023-12-05 16:41:15 +0100

    [+] Day 04 Part 2 complete

--- Day 04/Part 2/main.py
+++ Day 04/Part 2/main.py
@@ -2,7 +2,32 @@
 # https://adventofcode.com/2023/day/4#part2
 
 def main():
-    pass
+    nbr_scratchcards = []
+
+    with open("Day 04/puzzle_input.txt", "r") as file:
+        lines = file.readlines()
+
+    lines = [line.strip().split(": ")[-1] for line in lines]
+
+    for i in range(len(lines)):
+        nbr_scratchcards.append(1)
+
+    for i in range(len(lines)):
+        worth = 0
+
+        win_nbrs = lines[i].split(" | ")[0]
+        win_nbrs = [nbr for nbr in win_nbrs.split(" ") if nbr != '']
+        my_nbrs = lines[i].split(" | ")[1]
+        my_nbrs = [nbr for nbr in my_nbrs.split(" ") if nbr != '']
+
+        for j in range(len(my_nbrs)):
+            if my_nbrs[j] in win_nbrs:
+                worth += 1
+
+        for x in range(1, worth + 1):
+            nbr_scratchcards[i + x] += nbr_scratchcards[i]
+
+    print(sum(nbr_scratchcards))
 
 if __name__ == "__main__":
     main()
