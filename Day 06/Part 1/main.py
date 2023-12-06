6f5ef00c9d39c9a9f0800f3d19805d1c9c22c547
Rigomont <elouan.rigomont@epitech.eu>
2023-12-06 11:01:24 +0100

    [+] Day 06 Complete

--- Day 06/Part 1/main.py
+++ Day 06/Part 1/main.py
@@ -1,8 +1,42 @@
 # Advent of Code 2023 - Day 6 - Part 1
 # https://adventofcode.com/2023/day/6#part1
 
+def get_nbr_ways(time, distance):
+    nbr_ways = 0
+
+    for i in range(int(time) + 1):
+        rest = int(time) - i
+        d = rest * i
+
+        if d > int(distance):
+            nbr_ways += 1
+
+    return nbr_ways
+
+def multiply_list(lst):
+    result = 1
+
+    for num in lst:
+        result *= num
+
+    return result
+
 def main():
-    pass
+    all_ways = []
+
+    with open("Day 06/puzzle_input.txt", "r") as file:
+        lines = file.readlines()
+
+    lines = [line.strip() for line in lines]
+    lines = [line.split(":") for line in lines]
+
+    times = [time for time in lines[0][1].split(" ") if time]
+    distances = [distance for distance in lines[1][1].split(" ") if distance]
+
+    for i in range(len(times)):
+        all_ways.append(get_nbr_ways(times[i], distances[i]))
+
+    print(multiply_list(all_ways))
 
 if __name__ == "__main__":
     main()
--- Day 06/Part 2/main.py
+++ Day 06/Part 2/main.py
@@ -1,8 +1,35 @@
 # Advent of Code 2023 - Day 6 - Part 2
 # https://adventofcode.com/2023/day/6#part2
 
+def get_nbr_ways(time, distance):
+    nbr_ways = 0
+
+    for i in range(int(time) + 1):
+        rest = int(time) - i
+        d = rest * i
+
+        if d > int(distance):
+            nbr_ways += 1
+
+    return nbr_ways
+
 def main():
-    pass
+    all_ways = None
+
+    with open("Day 06/puzzle_input.txt", "r") as file:
+        lines = file.readlines()
+
+    lines = [line.strip() for line in lines]
+    lines = [line.split(":") for line in lines]
+
+    times = [time for time in lines[0][1].split(" ") if time]
+    time = ''.join(times)
+    distances = [distance for distance in lines[1][1].split(" ") if distance]
+    distance = ''.join(distances)
+
+    all_ways = get_nbr_ways(time, distance)
+
+    print(all_ways)
 
 if __name__ == "__main__":
     main()
