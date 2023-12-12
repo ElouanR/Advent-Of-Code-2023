54df3c0e0d36f767d31aadcb200e53a59852da91
Rigomont <elouan.rigomont@epitech.eu>
2023-12-12 17:01:08 +0100

    [~] work on optimizations

--- Day 05/Part 2/main.py
+++ Day 05/Part 2/main.py
@@ -1,17 +1,10 @@
 # Advent of Code 2023 - Day 5 - Part 2
 # https://adventofcode.com/2023/day/5#part2
 
+from pprint import pprint
+
 def get_min_location(almanac, seeds_pairs):
-    min_location = None
-
-    for i in range(0, len(seeds_pairs) - 1, 2):
-        for x in range(int(seeds_pairs[i]), int(seeds_pairs[i]) + int(seeds_pairs[i + 1]) - 1):
-            location = get_location(almanac, str(x))
-
-            if (min_location == None or int(location) < min_location):
-                min_location = int(location)
-
-    return min_location
+    pass
 
 def init_almanac():
     almanac_names = ["seeds", "soil", "fertilizer", "water", "light", "temperature", "humidity", "location"]
@@ -33,7 +26,8 @@ def init_almanac():
 
     lines = [line.strip().split() for line in lines]
 
-    almanac[almanac_names[almanac_nbr]] = lines[0][1:]
+    for j in range(len(lines[0][1:]) - 1):
+        almanac[almanac_names[almanac_nbr]].append([lines[0][1:][j], lines[0][1:][j + 1]])
 
     while (i < len(lines)):
         if (lines[i] == []):
@@ -45,26 +39,23 @@ def init_almanac():
 
     return almanac
 
-def get_location(almanac, seed):
+def get_location(almanac, seeds_range):
     almanac_names = ["soil", "fertilizer", "water", "light", "temperature", "humidity", "location"]
-    source = seed
+    sources_range = seeds_range
 
     for x in range(len(almanac_names)):
-        for i in range(len(almanac[almanac_names[x]])):
-            nbr_destination = int(almanac[almanac_names[x]][i][0])
-            nbr_source = int(almanac[almanac_names[x]][i][1])
-            nbr_range = int(almanac[almanac_names[x]][i][2])
-
-            if (((nbr_source) <= int(source)) and ((nbr_source + nbr_range - 1) >= int(source))):
-                source = str(nbr_destination + int(source) - nbr_source)
-                break
-
-    return source
+        temp = []
+        for rangeee in sources_range:
+            for i in range(len(almanac[almanac_names[x]])):
+                nbr_destination = int(almanac[almanac_names[x]][i][0])
+                nbr_source = int(almanac[almanac_names[x]][i][1])
+                nbr_source_to = nbr_source + int(almanac[almanac_names[x]][i][2]) - 1
 
 def main():
     almanac = init_almanac()
 
-    print(get_min_location(almanac, almanac["seeds"]))
+    #print(get_min_location(almanac, almanac["seeds"]))
+    pprint(almanac)
 
 if __name__ == "__main__":
     main()
