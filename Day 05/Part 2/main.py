af2a158f0aa0b2720e1ed07ac559091a4349cac4
Rigomont <elouan.rigomont@epitech.eu>
2023-12-13 11:56:21 +0100

    [~] work on optimizations Day 05 Part 2

--- Day 05/Part 2/main.py
+++ Day 05/Part 2/main.py
@@ -3,8 +3,13 @@
 
 from pprint import pprint
 
-def get_min_location(almanac, seeds_pairs):
-    pass
+def get_min_location(almanac, seeds_ranges):
+    for seeds in seeds_ranges:
+        tmp = []
+        tmp.append(seeds)
+        tmp[0][0] = int(tmp[0][0])
+        tmp[0][1] = int(tmp[0][1])
+        get_location(almanac, tmp)
 
 def init_almanac():
     almanac_names = ["seeds", "soil", "fertilizer", "water", "light", "temperature", "humidity", "location"]
@@ -44,18 +49,40 @@ def get_location(almanac, seeds_range):
     sources_range = seeds_range
 
     for x in range(len(almanac_names)):
-        temp = []
-        for rangeee in sources_range:
+        tmp = []
+        while sources_range != []:
             for i in range(len(almanac[almanac_names[x]])):
                 nbr_destination = int(almanac[almanac_names[x]][i][0])
                 nbr_source = int(almanac[almanac_names[x]][i][1])
                 nbr_source_to = nbr_source + int(almanac[almanac_names[x]][i][2]) - 1
+                seeds_to = sources_range[0][0] + sources_range[0][1] - 1
+
+                overlap_point_start = max(nbr_source, sources_range[0][0])
+                overlap_point_end = min(nbr_source_to, seeds_to)
+
+                print(sources_range)
+                print(seeds_to)
+                print()
+                print("nbr_source: ", nbr_source)
+                print("nbr_source_to: ", nbr_source_to)
+                print()
+                print("overlap_point_start: ", overlap_point_start)
+                print("overlap_point_end: ", overlap_point_end)
+                print()
+
+                if (nbr_source <= sources_range[0][0] and nbr_source_to >= seeds_to):
+                    print("source_range in destination_range")
+
+                if (nbr_source > sources_range[0][0] and nbr_source_to < seeds_to):
+                    print("destination_range in source_range")
+
+                input()
 
 def main():
     almanac = init_almanac()
 
-    #print(get_min_location(almanac, almanac["seeds"]))
-    pprint(almanac)
+    get_min_location(almanac, almanac["seeds"])
+    #pprint(almanac)
 
 if __name__ == "__main__":
     main()
